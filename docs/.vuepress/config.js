3896fa8553fd13537dbe68bc588f0fc64f293e3d
DolaTRPG <dolatrpg@gmail.com>
2019-05-24 18:41:08 +0800

    建立基本框架

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'Night of the Dead TRPG',
+    description: 'Night of the Dead TRPG',
+    base: '/night-of-the-dead/',
+    repo: 'https://github.com/dolatrpg/night-of-the-dead',
+    themeConfig: {
+        sidebarDepth: 0,
+        sidebar:[
+            ['/', '系統簡介'],
+            {
+                title: '系統',
+                collapsable: false,
+                sidebarDepth: 2,
+                children: [
+                    '/system/character'
+                ]
+            }
+        ]
+    }
+}
