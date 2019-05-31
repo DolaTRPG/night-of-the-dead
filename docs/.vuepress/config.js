fd3441ae61317fdf275af7dd6ed28436150c0e79
DolaTRPG <dolatrpg@gmail.com>
2019-05-31 13:48:54 +0800

    修正頁面格式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,21 +4,19 @@ module.exports = {
     base: '/night-of-the-dead/',
     repo: 'https://github.com/dolatrpg/night-of-the-dead',
     themeConfig: {
-        sidebarDepth: 0,
+        sidebarDepth: 2,
         sidebar:[
             ['/', '系統簡介'],
             {
                 title: '系統',
                 collapsable: false,
-                sidebarDepth: 2,
                 children: [
                     '/system/character'
                 ]
             },
             {
                 title: '故事',
-                collapsable: true,
-                sidebarDepth: 2,
+                collapsable: false,
                 children: [
                     '/story/01.md'
                 ]
